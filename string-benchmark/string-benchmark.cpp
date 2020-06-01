6e2dcaa35e749fceee28aa4bcf56cdd7e0f2e452
Bu11etmagnet <Bu11etmagnet@users.noreply.github.com>
2020-06-01 17:57:10 +0200

    Add benchmark project

---
+++ string-benchmark/string-benchmark.cpp
@@ -0,0 +1,12 @@
+﻿// string-benchmark.cpp : Defines the entry point for the application.
+//
+
+#include "string-benchmark.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
